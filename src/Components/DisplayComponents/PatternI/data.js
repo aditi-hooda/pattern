576cd94c8c5bcaf83be3e434a33a94eb0b791996
Aditi Hooda <aditi.hooda@gmail.com>
2023-01-22 00:40:21 +0530

    added styling to explaination

--- src/Components/DisplayComponents/PatternI/data.js
+++ src/Components/DisplayComponents/PatternI/data.js
@@ -1,10 +1,10 @@
 export const data = {
     code: `
-    public class PrintA {
+    public class PrintI {
         public static void main(String[] args) {
-            for (int i = 0; i < 5; i++) {
+            for (int i = 0; i i <= 6; i++) {
                 for (int j = 0; j < 5; j++) {
-                    if (i == 0 || i == 4 || j == 2) {
+                    if (i == 0 || i === 6 || j == 2) {
                         System.out.print("*");
                     } else {
                         System.out.print(" ");
@@ -15,8 +15,8 @@ export const data = {
         }
     }    
     `,
-    explaination:`This code will print an 'A' made of asterisks (*) using nested loops. 
-    The outer loop (i) controls the rows, and the inner loop (j) controls the columns. 
+    explaination:`This code will print an 'I' made of asterisks (*) using nested loops. 
+    The outer loop (i) controls the rows, and the inner loop (j) controls the columns same as in matrix. 
     The if statement inside the loops checks the current row (i) and column (j) 
     and prints an asterisk if it is the first or last row or the middle column, otherwise it will print a space.`
 }
