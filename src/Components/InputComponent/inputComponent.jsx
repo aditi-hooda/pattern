2fd2d90232003c65688587bcca4f88ce43d51261
Aditi Hooda <aditi.hooda@gmail.com>
2023-01-21 20:46:10 +0530

    adding on click function to call functions based on charater with in string entered

--- src/Components/InputComponent/inputComponent.jsx
+++ src/Components/InputComponent/inputComponent.jsx
@@ -8,7 +8,88 @@ export default function InputComponent() {
     }
     function handleClick(event){
         //create pattern for each letter in input field
-           
+        var characters = inputValue.split("");
+        for(var ch in characters){
+            switch(ch){
+                case 'A':
+                    //call function to print A
+                break;
+                case 'B':
+                    //call function to print A
+                break;
+                case 'C':
+                    //call function to print A
+                break;
+                case 'D':
+                    //call function to print A
+                break;
+                case 'E':
+                    //call function to print A
+                break;
+                case 'F':
+                    //call function to print A
+                break;
+                case 'G':
+                    //call function to print A
+                break;
+                case 'H':
+                    //call function to print A
+                break;
+                case 'I':
+                    //call function to print A
+                break;
+                case 'J':
+                    //call function to print A
+                break;
+                case 'K':
+                    //call function to print A
+                break;
+                case 'L':
+                    //call function to print A
+                break;
+                case 'N':
+                    //call function to print A
+                break;
+                case 'O':
+                    //call function to print A
+                break;
+                case 'P':
+                    //call function to print A
+                break;
+                case 'Q':
+                    //call function to print A
+                break;
+                case 'R':
+                    //call function to print A
+                break;
+                case 'S':
+                    //call function to print A
+                break;
+                case 'T':
+                    //call function to print A
+                break;
+                case 'U':
+                    //call function to print A
+                break;
+                case 'V':
+                    //call function to print A
+                break;
+                case 'W':
+                    //call function to print A
+                break;
+                case 'X':
+                    //call function to print A
+                break;
+                case 'Y':
+                    //call function to print A
+                break;
+                case 'Z':
+                    //call function to print A
+                break;
+                default:
+                    alert("Invalid Character");
+            }
+        }
     }
 
     return (
